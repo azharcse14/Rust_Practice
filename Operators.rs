b9f33a610c869f86fdeb48c1ef4fe4f5a8f8098b
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2023-02-21 10:35:49 +0600

    Operators

---
+++ Operators.rs
@@ -0,0 +1,30 @@
+fn main(){
+    // 1+2
+    // arithmetic operator
+    println!("{}",1+2 );
+    println!("{}",1-2 );
+    println!("{}",1*2 );
+    println!("{}",1.0/2.0 );
+    println!("{}",11%2 );
+
+    // comparison operator
+    println!("{}",12<18);
+    println!("{}",12>18);
+    println!("{}",12==18);
+    println!("{}",12!=18);
+
+    /*
+    == 
+    =  let name = 12;
+    */
+    // logical operator
+    println!("{}",12 == 12 || 13 == 1 || 2 == 2);
+     println!("{}",!1==1);
+     let mut a = 12;
+     println!("{}",a);
+     a += 10;
+     println!("{}",a);
+
+    
+
+}
