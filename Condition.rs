1f58c2daf8d352057ae49b87b5dea3652b18303c
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2023-02-21 11:37:33 +0600

    Condition

---
+++ Condition.rs
@@ -0,0 +1,13 @@
+fn main(){
+    let a = 123;
+    let b = 121;
+    if a == b {
+        println!("OK");
+    }
+    else if a < b {
+        println!("no");
+    }
+    else{
+        println!("Jgdufg");
+    }
+}
