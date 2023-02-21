f3ec2e9bd1c26996d411d3c4a1cb429ea203afd9
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2023-02-21 10:24:47 +0600

    Mutable and Immutable Variable

---
+++ MutableAndImmutableVariable.rs
@@ -0,0 +1,11 @@
+fn main(){
+    //Immutable 
+    let name = "Kalua";
+    println!("{name}");
+    //Mutable
+    let mut age = 16;
+    println!("{age}");
+    age = 17;
+    println!("{age}");
+     
+}
