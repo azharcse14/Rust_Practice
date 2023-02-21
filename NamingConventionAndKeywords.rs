7c7af5d677c5ca486595f8bf6af83a79949d96ec
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2023-02-21 10:16:18 +0600

    Naming Convention & Keywords

---
+++ NamingConventionAndKeywords.rs
@@ -0,0 +1,29 @@
+fn main(){
+    //     let name_1_is = "Abhishek";
+    //     println!("{}",name_1_is);
+    //     let _a = 23;
+    //     let name_dfg_ = "ygilhku";
+    //     println!("{}",_a);
+    //     println!("{}",name_dfg_);
+    //     let _dig = 12;
+    //     println!("{}",_dig);
+    
+    //     let a = 45;
+    //     let A = 34;
+    //     println!("{}",a);
+    //     println!("{}",A);
+    
+    //     let HelloIamAzhar = 12;
+    //     println!("{}",HelloIamAzhar);
+    
+    
+        // Camel case (ex: someVariable )    ----> helloIAmAzhar
+        // Snake case (ex: some_variable) ----> hello_i_am_azhar
+        let camel_case = "camelCase";
+        let snake_case = "snake_cas";
+        println!("{}",camel_case);
+        println!("{}",snake_case);
+        let a_s = 12;
+        println!("{a_s}");
+    
+    }
