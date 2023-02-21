e758158b82db9f6f52de621a33f69c462a748461
MD. Azharul Islam <36479711+azharcse14@users.noreply.github.com>
2023-02-21 11:49:32 +0600

    Nested Condition

---
+++ NestedCondition.rs
@@ -0,0 +1,20 @@
+fn main(){
+    let value = false;
+    if value{
+        println!("Yes");
+    }
+    else{
+        println!("No");
+    }
+    if 1 == 2{
+        if 2 == 3{
+            println!("Pro");
+        }
+        else{
+            println!("SGHF");
+        }
+    }
+    else{
+        println!("jbkjbj");
+    }
+}
